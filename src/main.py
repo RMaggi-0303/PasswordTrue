9cfb69eedd1fd5f2fc1c7069073df811bfe013b1
RMaggi-0303 <riccardo.maggi0303@gmail.com>
2026-08-19 20:38:44 +0100

    updated main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import json
+from pathlib import Path
+
+password_file = Path("passwords.json")
+
+class Password:      #placeholder
+    password = ""
+    valid = False
+    strength = 0
+
+#TODO implement writing and reading of passwords from JSON files first
