f2739fd8b14dbaf2cb430df5d96703063e863b91
RMaggi-0303 <riccardo.maggi0303@gmail.com>
2026-08-27 16:41:30 +0100

    implemented basic menu

--- src/main.py
+++ src/main.py
@@ -13,8 +13,44 @@ def load_passwords():
     with password_file.open("r") as file:
         return json.load(file)
 
+def save_passwords(passwords):
+    with password_file.open("w") as file:
+        json.dump(passwords, file)
+
+def menu():
+    while True:
+        print("\n--- PasswordTrue ---\n")
+        print("1. View passwords")
+        print("2. Save password")
+        print("3. Generate random password")
+        print("4. Delete password")
+        print("5. Exit")
+
+        choice = input("\nSelect an option by typing a number:")
+
+        if choice == "1":
+            print("to be implemented")
+            print(passwords)
+            #view_passwords()
+        elif choice == "2":
+            print("to be implemented")
+            #save_password
+        elif choice == "3":
+            print("to be implemented")
+            #generate_password
+        elif choice == "4":
+            print("to be implemented")
+            #delete_password
+        elif choice == "5":
+            print("Your passwords are safe!")
+            break
+        else:
+            print("Invalid option.")
+
+
 passwords = load_passwords()
 
-print(passwords)
+menu()
 
 #TODO implement writing and reading of passwords from JSON files first
+#TODO implement menu functions
